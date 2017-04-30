ac0a2483d59c1b4175a4768d4cc94c8163a67033
Alexander Klimik <sanyakl242@gmail.com>
2017-04-30 19:59:11 +0300

    20:00 commit

---
+++ js/common.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+    $('.team__cards').bxSlider({
+        controls: false,
+        keyboardEnabled: true
+    });
+
+    // $('.testimonials__cards').bxSlider({
+    //     controls: false,
+    //     keyboardEnabled: true,
+    //     auto: true
+    // });
+
+    $('.testimonials__cards').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false
+    });
+
+    smoothScroll.init({
+        selector: '[data-scroll]',
+        selectorHeader: null,
+        speed: 500,
+        easing: 'ease',
+        offset: 0
+    });
+
+    let grid = $('.portfolio-works').isotope({
+        itemSelector: '.portfolio-works__item',
+        layoutMode: 'masonry',
+        masonry: {
+            horizontalOrder: true
+        }
+    });
+
+    $('.portfolio__filter').click(function() {
+        let filterValue = $( this ).attr('data-filter');
+        grid.isotope({ filter: filterValue });
+
+        $(".portfolio__filter").removeClass("active-filter");
+        $(this).addClass("active-filter");
+    });
+
+    $('.top-menu__item').click(function() {
+        $(".top-menu__item").removeClass("active-menu-link");
+        $(this).addClass("active-menu-link");
+    });
+});
+
+;(function () {
+    window.onload = function () {
+        let map,
+            point = {lat: 48.7268145, lng: 37.6019125},
+            iv1Content = document.querySelector('.info-window-1');
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: point,
+                zoom: 17
+            });
+            let marker = new google.maps.Marker({
+                position: point,
+                map: map
+            });
+            // let infowindow = new google.maps.InfoWindow({
+            //     content: iv1Content
+            // });
+            // marker.addListener('click', function () {
+            //     infowindow.open(map, marker);
+            //     iv1Content.style.opacity = 1;
+            // });
+        }
+
+        initMap();
+    }
+})();
